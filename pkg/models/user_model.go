5efa8a253c200342dd7804af4fa15b2b926895f0
Brandon Bermudez <brandonmorales555@gmail.com>
2021-04-19 21:19:43 -0600

    fix: change model

--- pkg/models/user_model.go
+++ pkg/models/user_model.go
@@ -4,9 +4,9 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	FirstName string
-	LastName  string
-	Email     string
-	Username  string
-	Password  string
+	FirstName string `json:"firstName" gorm:"size:100;not null"`
+	LastName  string `json:"lastName" gorm:"size:100;not null"`
+	Email     string `json:"email" gorm:"size:250;not null;unique"`
+	Username  string `json:"username" gorm:"size:100;not null;unique;index"`
+	Password  string `json:"password,omitempty" gorm:"size:300;not null"`
 }
