09e3739048a9f6bbdba351ae8dbb0760d98461fe
Brandon Bermudez <brandonmorales555@gmail.com>
2021-04-20 22:05:05 -0600

    fix(main): entrypoint updated

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
+	"github.com/wtfcode/hermes-api/pkg/database"
+	"github.com/wtfcode/hermes-api/pkg/server"
 )
 
 func main() {
@@ -11,4 +14,15 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
+	db := database.Connect()
+	database.Migrate(db)
+
+	PORT := os.Getenv("PORT")
+	if len(PORT) == 0 {
+		PORT = "7000"
+	}
+
+	app := server.NewServer(db)
+	app.Listen(":" + PORT)
 }
